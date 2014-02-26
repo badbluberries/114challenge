c17afcf37e15d99f76b80d4a370d744942667ea6
badbluberries <colinmckinstry@colins-mbp.home>
2014-02-26 20:27:56 +0000

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Colin McKinstry on 26/02/2014.
+//  Copyright (c) 2014 Colin McKinstry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
